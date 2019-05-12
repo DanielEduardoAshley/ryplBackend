4e2d7708b303b88cdb008d5f823d76919a01eade
Syed Aziz <syedaziz27x@gmail.com>
2019-05-12 18:33:42 -0400

    made sql file

---
+++ seed.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS rypl;
+CREATE DATABASE rypl;
+
+\c rypl
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    firebase_uid VARCHAR NOT NULL,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    picture_url VARCHAR
+);
+
+CREATE TABLE category
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE video
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    category_name INT REFERENCES category(id),
+    video_title VARCHAR NOT NULL,
+    video_url VARCHAR NOT NULL,
+    annotation VARCHAR,
+    description VARCHAR,
+    time_posted TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE response
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    video_id INT REFERENCES video(id),
+    video_title VARCHAR NOT NULL,
+    annotation VARCHAR,
+    description VARCHAR,
+    time_posted TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE response_to_response
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    video_id INT REFERENCES response(id),
+    video_title VARCHAR NOT NULL,
+    annotation VARCHAR,
+    description VARCHAR,
+    time_posted TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE followers
+(
+    id SERIAL PRIMARY KEY,
+    follower INT REFERENCES users(id),
+    following INT REFERENCES users(id)
+);
+
+-- INSERT INTO users
+--     (username, email,firebase_uid,firstname,lastname,picture_url)
+-- VALUES
+--     ('a', 'b', 'c', 'd', 'e', 'f');
+
+-- INSERT INTO users
+--     (username, email,firebase_uid,firstname,lastname,picture_url)
+-- VALUES
+--     ('aa', 'bb', 'cc', 'dd', 'ee', 'ff');
+
+-- INSERT INTO followers
+--     (follower)
+-- VALUES
+--     (1);
+
+-- INSERT INTO video
+--     (user_id,video_title,video_url)
+-- VALUES
+--     (1, 'abc', 'def');
+
+-- INSERT INTO response
+--     (user_id,video_id,video_title)
+-- VALUES
+--     (1, 1, 'abc');
+
+-- INSERT INTO response_to_response
+--     (video_id, video_title)
+-- VALUES
+--     (1, 'ew')
+
+
+
+
+
+
+
+
