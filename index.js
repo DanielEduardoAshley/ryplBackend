c263e6fef7fb10715bbce656c2f2c023889e8045
Syed Aziz <syedaziz27x@gmail.com>
2019-06-02 14:58:04 -0400

    changed port so yun can use

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { app } = require("./app");
+const {
+  app
+} = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
   console.log("listening on port:" + port);
